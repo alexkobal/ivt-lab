1801bd494efe87d83ac009ff8b1a4a9bf9909f32
Sándor Kobál <alexkobal@edu.bme.hu>
2020-05-22 21:03:34 +0200

    Mocks added

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,45 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore storeOneMock;
+  private TorpedoStore storeTwoMock;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    storeOneMock = mock(TorpedoStore.class);
+    storeTwoMock = mock(TorpedoStore.class);
+    this.ship = new GT4500(storeOneMock, storeTwoMock);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(storeOneMock.isEmpty()).thenReturn(false);
+    when(storeOneMock.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(storeOneMock, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(storeOneMock.isEmpty()).thenReturn(false);
+    when(storeTwoMock.isEmpty()).thenReturn(false);
+    when(storeOneMock.fire(1)).thenReturn(true);
+    when(storeTwoMock.fire(2)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(storeOneMock, times(1)).fire(1);
+    verify(storeTwoMock, times(1)).fire(2);
   }
 
 }
